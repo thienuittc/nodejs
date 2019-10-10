2e1395c699990463acf98b565c5cd8ba16d7b9e7
trancongthien <thienuittc@gmail.com>
2019-10-11 05:05:03 +0700

    make it better

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ webapp_nsp.on('connection', function(socket) {
 	//Khi webapp socket bị mất kết nối
 	socket.on('disconnect', function() {
     User.create({
-      name:'thien',
+      name:'thien1',
       age :15
     });
 		console.log("Disconnect socket webapp")
